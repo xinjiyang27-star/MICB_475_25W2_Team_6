37f079cd830b641f812f5872a9695a3f4788951c
carolinewei1012 <125171404+carolinewei1012@users.noreply.github.com>
2026-03-04 13:44:05 -0800

    Add files via upload
    Updated ISA

--- Lab_Notebook/Indicator_Species_Analysis/ISA.R
+++ Lab_Notebook/Indicator_Species_Analysis/ISA.R
@@ -24,11 +24,68 @@ taxtable <- tax_table(ryan_filt) %>% as.data.frame() %>% rownames_to_column(var=
 
 # consider that your table is only going to be resolved up to the genus level, be wary of anything beyond the glomed taxa level
 # Combine the taxanomic info to the list of indicator species
-isa_table <- isa_ryan$sign %>%
+isa_table_all <- isa_ryan$sign %>%
   rownames_to_column(var="ASV") %>%
   left_join(taxtable) %>%
-  filter(p.value < 0.05)
-view(isa_table)
+  filter(p.value<0.05)
+View(isa_table_all)
+
+# Filter out ASVs that are shared between UC+healthy and CD+healthy
+isa_table_unique_UCCD <- isa_table_all %>%
+  filter(
+    !(
+      (s.Healthy == 1 & `s.Ulcerative Colitis` == 1 & `s.Crohn's Disease` == 0) |  # Healthy+UC only
+        (s.Healthy == 1 & `s.Crohn's Disease` == 1 & `s.Ulcerative Colitis` == 0)    # Healthy+CD only
+    )
+  ) 
+View(isa_table_unique_UCCD)
+
+# Create a new column that labels each ASV based on which condition(s) it belongs to.
+isa_unique_uccd_labeled <- isa_table_unique_UCCD %>%
+  mutate(group_label = case_when(
+    s.Healthy == 0 & `s.Ulcerative Colitis` == 1 & `s.Crohn's Disease` == 0 ~ "UC_only",
+    s.Healthy == 0 & `s.Ulcerative Colitis` == 0 & `s.Crohn's Disease` == 1 ~ "CD_only",
+    s.Healthy == 0 & `s.Ulcerative Colitis` == 1 & `s.Crohn's Disease` == 1 ~ "UC+CD_shared",
+    s.Healthy == 1 & `s.Ulcerative Colitis` == 1 & `s.Crohn's Disease` == 1 ~ "Healthy+UC+CD",
+    s.Healthy == 1 & `s.Ulcerative Colitis` == 0 & `s.Crohn's Disease` == 0 ~ "Healthy_only",
+    TRUE ~ "Other"
+  ))
+
+# Extracts the ASV IDs into separate lists based on the group category
+asv_uc_only  <- isa_unique_uccd_labeled %>% filter(group_label == "UC_only") %>% pull(ASV)
+asv_cd_only  <- isa_unique_uccd_labeled %>% filter(group_label == "CD_only") %>% pull(ASV)
+asv_uc_cd    <- isa_unique_uccd_labeled %>% filter(group_label == "UC+CD_shared") %>% pull(ASV)
+asv_all3     <- isa_unique_uccd_labeled %>% filter(group_label == "Healthy+UC+CD") %>% pull(ASV)
+
+asv_uc_only
+asv_cd_only
+asv_uc_cd
+asv_all3
 
 # Save the table
-write.csv(isa_table, "ISA_significant_taxa.csv", row.names = FALSE)
+write.csv(isa_table_unique_UCCD, "ISA_significant_taxa_unique_uccd.csv", row.names = FALSE)
+
+# Create a table only contains ASVs related to CD and Healthy
+isa_CD_Healthy <- isa_table_all %>%
+  filter(`s.Ulcerative Colitis` == 0)
+View(isa_CD_Healthy)
+
+# Create a new column that labels each ASV based on which condition(s) it belongs to.
+isa_CD_Healthy_labeled <- isa_CD_Healthy %>%
+  mutate(group_label = case_when(
+    s.Healthy == 1 & `s.Crohn's Disease` == 0 ~ "Healthy_only",
+    s.Healthy == 0 & `s.Crohn's Disease` == 1 ~ "CD_only",
+    s.Healthy == 1 & `s.Crohn's Disease` == 1 ~ "CD+Healthy_shared"
+  ))
+
+# Extracts the ASV IDs into separate lists based on the group category
+asv_cd_only <- isa_CD_Healthy_labeled %>% filter(group_label == "CD_only") %>% pull(ASV)
+asv_healthy_only <- isa_CD_Healthy_labeled %>% filter(group_label == "Healthy_only") %>% pull(ASV)
+asv_cd_healthy <- isa_CD_Healthy_labeled %>% filter(group_label == "CD+Healthy_shared") %>% pull(ASV)
+
+asv_cd_only
+asv_healthy_only
+asv_cd_healthy
+
+# Save the table
+write.csv(isa_CD_Healthy, "ISA_significant_taxa_cd_healthy.csv", row.names = FALSE)
