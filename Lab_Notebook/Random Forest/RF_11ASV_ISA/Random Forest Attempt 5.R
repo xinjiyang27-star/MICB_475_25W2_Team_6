036c20df08bad3b486371968cc02707577b3437f
carolinewei1012 <125171404+carolinewei1012@users.noreply.github.com>
2026-03-12 10:33:49 -0700

    Add files via upload

---
+++ Lab_Notebook/Random Forest/RF_11ASV_ISA/Random Forest Attempt 5.R
@@ -0,0 +1,179 @@
+#Load Libraries
+library(randomForest)
+library(caret)
+library(ranger)
+library(pROC)
+library(boot)
+library(ggplot2)
+library(phyloseq)
+library(tidyverse)
+
+# Load the dataset and aggregate reads to the Genus level
+load("ryan_filt_new.RData")
+ps <- ryan_filt_new
+
+# Convert taxonomy table to matrix and assign genus to your ASV
+tax_mat <- as(tax_table(ps), "matrix")
+tax_mat["71cdfae8e8fa2e8918f5db97f0fe52ab", "Genus"] <- "g__Azovibrio"
+tax_mat["7f83ae4b4bd781e53f9f26bf3e6c5680", "Genus"] <- "g__Senimuribacter"
+
+# Reassign the updated taxonomy table to the phyloseq object
+tax_table(ps) <- tax_table(tax_mat)
+
+# Check that the taxonomy table is valid
+tax_table(ps)["71cdfae8e8fa2e8918f5db97f0fe52ab", ]
+
+# Aggregate reads to Genus level
+ps <- tax_glom(ps,"Genus")
+
+# Load the indicator taxa results
+filtered_data<- read.csv("ISA_onlyCD.csv")
+
+# Save the filtered file
+write.csv(filtered_data, "filtered_data.csv", row.names = FALSE)
+
+# Modify the Genus column
+filtered_data_with_genus <- filtered_data %>%
+  mutate(Genus = case_when(
+    ASV == "71cdfae8e8fa2e8918f5db97f0fe52ab" ~ "g__Azovibrio",
+    ASV == "7f83ae4b4bd781e53f9f26bf3e6c5680" ~ "g__Senimuribacter",
+    TRUE ~ Genus
+  ))
+
+# Save the revised file
+write.csv(filtered_data_with_genus, "filtered_data_with_genus.csv", row.names = FALSE)
+
+# Normalize microbiome data and select only the significant taxa
+# CLR transformation
+ps_clr = ps %>% microbiome::transform('clr') 
+# Filter taxa
+ps_filt = prune_taxa(filtered_data_with_genus$ASV,ps_clr)
+
+# Make sure ps_filt uses the curated Genus names
+tax_table(ps_filt)[, "Genus"] <- filtered_data_with_genus$Genus[
+  match(rownames(tax_table(ps_filt)), filtered_data_with_genus$ASV)
+]
+# Melt the dataset
+df = psmelt(ps_filt) %>% 
+  # dashes will cause errors
+  mutate(Condition = str_replace_all(Condition,"Crohn's Disease","Crohns_Disease"))
+
+# Add Z transformation to each Genus individually
+# (mean of zero, standard deviation of 1)
+df_transformed = df %>% 
+  group_by(Genus) %>% 
+  mutate(Abundance = scale(Abundance)) %>% 
+  ungroup()
+
+# Filter For Conditions
+target_conditions <- c("Crohns_Disease", "Healthy")
+filtered_df <- df_transformed %>% 
+  filter(Condition %in% target_conditions)
+
+# Final table should ONLY contain the outcome and explanatory variables, each as their own column. For now we'll also include the sample id.
+df_pivot = filtered_df %>% 
+  select(Sample,Condition,Genus,Abundance) %>% 
+  # Turn each Genus into its own column
+  pivot_wider(names_from = Genus, values_from = Abundance)
+
+# Remove rows with NA values in the metadata
+df_noNA = df_pivot %>% na.omit()
+
+# Remove the sample ID column - otherwise the code will try to use it as an explanatory variable (just like the microbial genera).
+# We do this after pivoting (try doing it before pivoting, see what happens!)
+df_final = df_noNA %>% select(-Sample)
+
+#Set Predictors
+predictors = df_final %>% select(-Condition)
+
+# We will transform subject into a factor.
+# The first level of the factor is automatically used as the reference group.
+# We will put subject_1 first.
+# The results will describe subject 2 relative to the reference (subject_1).
+outcome = df_final %>% pull(Condition) %>% 
+  factor(levels = c("Crohns_Disease","Healthy"))
+
+# Randomly subsets the rows into k equal bins.
+k = 10
+set.seed(066)
+folds = createFolds(outcome, k = k, list = TRUE)
+
+# Each of these folds will take a turn being the test dataset.
+str(folds)
+#Hyperparameter Tuning
+# mtry: number of variables that will be used per forest. 
+#       High = overfitting, low = uninformative
+
+# splitrule: affects how decision trees are calculated.
+#            Use gini or extratrees for boolean outcomes (ex. subject)
+#            Use variance for continuous outcomes (ex. age)
+
+# min.node.size: Related to tree complexity. Larger = simpler tree.
+# Often best as a proportional fraction of your sample size.
+
+# These are generic values. Depending on your dataset, you may need to adjust the numeric ranges up or down.
+tune_grid = expand.grid(mtry = c(5,7,10), 
+                        splitrule = c("gini","extratrees"),
+                        min.node.size = c(21,42,83))
+
+#Run RF
+source('randomforest_functions.R')
+pd_model = run_rf(X = predictors, y = outcome, 
+                  fold_list = folds,
+                  hyper = tune_grid, 
+                  rngseed = 066)
+names(pd_model)
+
+#Interpretation
+
+roc_test = roc(pd_model$test_labels$true_labels,
+               pd_model$test_labels$predicted_probabilities)
+roc_train = roc(pd_model$train_labels$true_labels,
+                pd_model$train_labels$predicted_probabilities)
+roc_plot <- ggplot() +
+  # Training data: this is a type of control
+  geom_line(aes(x = 1 - roc_train$specificities, 
+                y = roc_train$sensitivities), 
+            color = "red",size=1) +
+  # Test data: tells us the strength of the prediction
+  geom_line(aes(x = 1 - roc_test$specificities,
+                y = roc_test$sensitivities), 
+            color = "black",size=1) +
+  geom_abline(slope = 1, intercept = 0, color = "gray", linetype = "dashed",size=1) +
+  labs(x = "False Positive Rate", y = "True Positive Rate") +
+  annotate("text", x = 0.7, y = 0.2, 
+           label = sprintf("Train (red): %.2f (%.2f-%.2f)\nTest (black): %.2f (%.2f-%.2f)",
+                           auc(roc_train), pd_model$auc_train_ci[1], pd_model$auc_train_ci[2],
+                           auc(roc_test), pd_model$auc_test_ci[1], pd_model$auc_test_ci[2]), 
+           size = 6) +
+  theme_minimal(base_size=18)
+
+pd_model$importance
+
+ggsave("ROC_curve.png", roc_plot, width = 8, height = 6, dpi = 300)
+
+importance_plot <- pd_model$importance %>% 
+  # Data are automatically arranged by decreasing importance - turn it into a factor.
+  # Otherwise the features will show up alphabetically in the plot.
+  mutate(Feature = factor(.$Feature,levels = .$Feature)) %>% 
+  ggplot(aes(Feature,MeanDecreaseGini,fill=MeanDecreaseGini)) +
+  geom_col() +
+  theme_classic(base_size=18) +
+  theme(axis.text.x = element_text(angle=70, vjust = 1, hjust=1)) +
+  ylab('Importance (Gini)') + xlab(NULL)
+
+ggsave("RF_feature_importance.png", importance_plot, width = 8, height = 6, dpi = 300)
+
+#Check Correlation
+
+temp = ps_clr %>% 
+  subset_taxa(Genus=='g__Anaerococcus') %>% 
+  psmelt()
+
+box_plot_RF <- temp %>% 
+  ggplot(aes(Condition,Abundance)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(height=2, width = 0.2) +
+  theme_classic(base_size=18)
+
+ggsave("Anaerococcus_abundance.png", box_plot_RF, width = 8, height = 6, dpi = 300)
