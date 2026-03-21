e83f1be5b6e5ec02a8e2a60b887ae8603e692f2f
egbertxxx <xinjiyang27@gmail.com>
2026-03-21 11:57:06 +0800

    Add files via upload

---
+++ Lab_Notebook/Final_figure_analysis/Analysis/Filtered_alpha/Significance.R
@@ -0,0 +1,100 @@
+# Significance Analysis for Alpha Diversity Metrics
+
+# Load the packages 
+library(phyloseq)
+library(ape)
+library(tidyverse)
+library(vegan)
+library(picante)
+library(ggsignif)
+library(ggpubr)
+
+# Load the relevant items in the RData file 
+load("Alpha_formatted.RData")
+
+# Calculate alpha diversity and merge with the metadata
+alphadiv <- estimate_richness(ryan_rare_new)
+samp_dat <- sample_data(ryan_rare_new)
+samp_dat_wdiv <- data.frame(samp_dat, alphadiv)
+
+#### Shannon Figure####  
+# Wilcoxon Rank Sum Test 
+wilcox.test(Shannon ~ Condition, data=samp_dat_wdiv, exact = FALSE)
+
+# Figure generation 
+alpha_plot_shannon <- ggplot(samp_dat_wdiv, aes(x = Condition,
+                                                y = Shannon)) +
+  stat_boxplot(geom = "errorbar", width = 0.2) +
+  geom_boxplot(aes(fill=Condition)) +
+  scale_fill_manual(values = c(
+    "Healthy" = "#7570B3",
+    "Crohn's Disease" = "#D55E00"
+  )) +
+  labs(x = "Condition", y = "Shannon Diversity Measure") + 
+  geom_signif(comparisons = list(c("Healthy", "Crohn's Disease")), 
+             test = "wilcox.test",
+             map_signif_level = TRUE, 
+             y_position = 4.6) +
+  theme_classic(base_size =13) + 
+  theme(legend.position = "none")
+alpha_plot_shannon
+
+# Save the figure 
+ggsave(filename = "plot_Shannon_sig.png"
+       , alpha_plot_shannon
+       , height=4, width=6)
+
+#### Observed Features Figure####
+# Wilcoxon Rank Sum Test 
+wilcox.test(Observed ~ Condition, data=samp_dat_wdiv, exact = FALSE)
+
+# Figure generation 
+alpha_plot_observed <- ggplot(samp_dat_wdiv, aes(x = Condition,
+                                                 y = Observed)) +
+  stat_boxplot(geom = "errorbar", width = 0.2) +
+  geom_boxplot(aes(fill=Condition)) +
+  scale_fill_manual(values = c(
+    "Healthy" = "#7570B3",
+    "Crohn's Disease" = "#D55E00"
+  )) +
+  labs(x = "Condition", y = "Observed Features Diversity Measure") + 
+  geom_signif(comparisons = list(c("Healthy", "Crohn's Disease")), 
+              test = "wilcox.test",
+              map_signif_level = TRUE, 
+              y_position = 300) +
+  theme_classic(base_size =13) + 
+  theme(legend.position = "none")
+alpha_plot_observed
+
+# Export the figure 
+ggsave(filename = "plot_Observed_sig.png"
+       , alpha_plot_observed
+       , height=4, width=6)
+
+#### PD Figure ####
+# faith PD calculation 
+phylo_dist <- pd(t(otu_table(ryan_rare_new)), phy_tree(ryan_rare_new),
+                 include.root=F) 
+
+samp_dat_wdiv$PD <- phylo_dist$PD
+
+# Wilcoxon Rank Sum Test 
+wilcox.test(PD ~ Condition, data=samp_dat_wdiv, exact = FALSE)
+
+# Figure generation 
+alpha_plot_pd <- ggplot(samp_dat_wdiv, aes(x = Condition,
+                                                        y = PD)) +
+  stat_boxplot(geom = "errorbar", width = 0.2) +
+  geom_boxplot(aes(fill=Condition)) +
+  scale_fill_manual(values = c(
+    "Healthy" = "#7570B3",
+    "Crohn's Disease" = "#D55E00"
+  )) +
+  labs(x = "Condition", y = "Faith's Phylogenetic Diversity Measure") + 
+  geom_signif(comparisons = list(c("Healthy", "Crohn's Disease")), 
+              test = "wilcox.test",
+              map_signif_level = TRUE, 
+              y_position = 16) +
+  theme_classic(base_size = 13) + 
+  theme(legend.position = "none")
+alpha_plot_pd
